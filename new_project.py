c7494dcd114be4b46bc91a115d58bfc5edd9cbf1
Marco <marco.pecci@stud.itsaltoadriatico.it>
2024-03-20 15:36:26 +0100

    esercizio censura parole

--- new_project.py
+++ new_project.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+def uncensor(frase, vocali):
+    output = "".join(vocali[idx] if lettera == '*' else lettera
+                     for idx, lettera in enumerate(frase))
+    output = []
+    idx = 0
+    for char in frase:
+        if char == '*':
+            output.append(vocali[idx])
+            idx += 1
+        else:
+            output.append(char)
+
+    return "".join(output)
+
+
+uncensor("H*ll* W*rld", "eoo")
